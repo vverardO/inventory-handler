8ba09df639069385e40c1486953f1394ea2241b1
vverardO <vb.verardo@gmail.com>
2022-09-28 21:53:07 -0300

    added dashboard;

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,10 +2,55 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use App\Models\ProductEntry;
+use App\Models\ProductMovimentation;
+use App\Models\ProductOutput;
+use App\Observers\ProductEntryObserver;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $warningProducts;
+    public $largestMovimentations;
+    public $newEntries;
+    public $newOutputs;
+    public $newMovimentations;
+    public $newProducts;
+
+    public function mount()
+    {
+        $this->warningProducts = DB::table('place_product')
+            ->select(
+                'products.name',
+                'place_product.quantity',
+                'places.name as place',
+                'products.minimum_amount as warning'
+            )->join('products', 'place_product.product_id', '=', 'products.id')
+            ->join('places', 'place_product.place_id', '=', 'places.id')
+            ->where('place_product.quantity', '<', 'products.minimum_amount')
+            ->orderBy('place_product.quantity')
+            ->get();
+
+        $this->largestMovimentations = DB::table('product_movimentations')
+            ->select(
+                'product_movimentations.quantity',
+                'products.name',
+                'users.name as user',
+            )->join('products', 'product_movimentations.product_id', '=', 'products.id')
+            ->join('users', 'product_movimentations.user_id', '=', 'users.id')
+            ->orderBy('product_movimentations.quantity', 'desc')
+            ->limit(5)
+            ->get();
+
+        $this->newEntries = ProductEntry::where('created_at', Carbon::today())->count();
+        $this->newOutputs = ProductOutput::where('created_at', Carbon::today())->count();
+        $this->newMovimentations = ProductMovimentation::where('created_at', Carbon::today())->count();
+        $this->newProducts = Product::where('created_at', Carbon::today())->count();
+    }
+
     public function render()
     {
         return view('livewire.dashboard');
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,18 +2,114 @@
 @section('page.title', 'Dashboard')
 
 <div class="row justify-content-center">
-    <div class="col-lg-5">
-        <div class="text-center mb-5">
-            <h2>Dashboard</h2>
-            <p class="text-muted mb-4" style="font-size: 25px;">Interface ainda não finalizada, ainda necessita definir os gráficos que vão ser apresentados aqui.</p>
-        </div>
-        @if(auth()->user()->mustEditProfile)
-        <div class="text-center mb-5">
-            <div class="alert alert-warning" role="alert">
-                <h2>Acesso</h2>
-                Pelo visto você está sem acesso aos menus, <a href="{{route("users.edit", auth()->user()->id)}}" class="alert-link">clique aqui</a> para alterar seus acessos.
+    <div class="col-xl-12">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-success alert-label-icon label-arrow" role="alert">
+                            <i class="fas fa-download label-icon"></i><strong>Entradas</strong> - {{$newEntries}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-danger alert-label-icon label-arrow" role="alert">
+                            <i class="fas fa-upload label-icon"></i><strong>Saídas</strong> - {{$newOutputs}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-primary alert-label-icon label-arrow" role="alert">
+                            <i class="fas fa-box-open label-icon"></i><strong>Produtos Novos</strong> - {{$newProducts}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-primary alert-label-icon label-arrow" role="alert">
+                            <i class="fas fa-exchange-alt label-icon"></i><strong>Movimentações</strong> - {{$newMovimentations}}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        @endif
+    </div>
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex flex-wrap align-items-center">
+                    <h5 class="card-title mb-0">Produtos com a quantidade crítica</h5>
+                </div>
+            </div>
+            <div class="card-body pt-xl-1">
+                <div class="table-responsive">
+                    <table class="table table-striped table-centered align-middle table-nowrap mb-0">
+                        <thead>
+                            <tr>
+                                <th>Nome do Produto</th>
+                                <th>Local</th>
+                                <th>Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($warningProducts as $product)
+                            <tr>
+                                <td>{{$product->name}}</td>
+                                <td>{{$product->place}}</td>
+                                <td>{{$product->quantity}}</td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="5" align="center">Nenhuma informação a ser apresentada</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex flex-wrap align-items-center">
+                    <h5 class="card-title mb-0">Top 5 maiores movimentações</h5>
+                </div>
+            </div>
+            <div class="card-body pt-xl-1">
+                <div class="table-responsive">
+                    <table class="table table-striped table-centered align-middle table-nowrap mb-0">
+                        <thead>
+                            <tr>
+                                <th>Nome do Produto</th>
+                                <th>Quantidade</th>
+                                <th>Usuário</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($largestMovimentations as $movimentation)
+                            <tr>
+                                <td>{{$movimentation->name}}</td>
+                                <td>{{$movimentation->quantity}}</td>
+                                <td>{{$movimentation->user}}</td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="3" align="center">Nenhuma informação a ser apresentada</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
